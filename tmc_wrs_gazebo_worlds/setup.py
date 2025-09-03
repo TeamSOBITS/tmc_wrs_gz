a396605b013e61f56aca514f592f79802418d067
MrKeith99 <kvalentincardenas@hotmail.com>
2025-09-04 08:55:56 +0900

    [UPD] compiler ros2

--- tmc_wrs_gazebo_worlds/setup.py
+++ tmc_wrs_gazebo_worlds/setup.py
@@ -1,11 +1,33 @@
 ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+from setuptools import setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'tmc_wrs_gz_worlds'
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['tmc_wrs_gazebo_worlds'],
-    package_dir={'': 'src'})
-
-setup(**setup_args)
+setup(
+    name=package_name,
+    version='0.21.2',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Yosuke Matsusaka, Nobuyuki Matsuno',
+    maintainer='Keith Valentin',
+    maintainer_email='kvalentincardenas@gmail.com',
+    description='World and model files for WRS Gazebo simulator.',
+    license='BSD 3-clause Clear License',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'spawn_objects = tmc_wrs_gz_worlds.spawn_objects:main',
+            'randomizer = tmc_wrs_gz_worlds.randomizer:main',
+        ],
+    },
+)
